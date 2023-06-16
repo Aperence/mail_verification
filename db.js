117979636e91e27d138de40f794018f597b1ce74
Aperence <anthony.doeraene@hotmail.com>
2023-06-16 19:09:55 +0200

    get user

--- db.js
+++ db.js
@@ -43,23 +43,6 @@ async function check_banned(hashed_mail){
 }
 
 /**
- * Remove all users with a given mail from the database
- * @param {string} hashed_mail : the mail of users
- * @returns a list of users deleted, with format [{"pseudo" : pseudo, "mail" : hashed_mail}]
- */
-async function remove_user(hashed_mail){
-
-    const db = client.db('sinf_server');
-    const collection = db.collection('users');
-
-    const users = await collection.find({"mail" : hashed_mail}).toArray()
-
-    await collection.deleteMany({"mail" : mail})
-
-    return users
-}
-
-/**
  * Add an hashed email to the banned database
  * @param {string} hashed_mail : the mail to add as banned
  */
@@ -84,6 +67,37 @@ async function add_user(mail, pseudo){
     console.log(hashed_mail)
 
     await collection.insertOne({"pseudo" : pseudo, "mail" : hashed_mail})
+}
+
+
+/**
+ * Remove all users with a given mail from the database
+ * @param {string} hashed_mail : the mail of users
+ * @returns a list of users deleted, with format [{"pseudo" : pseudo, "mail" : hashed_mail}]
+ */
+async function remove_user(hashed_mail){
+
+    const db = client.db('sinf_server');
+    const collection = db.collection('users');
+
+    const users = await collection.find({"mail" : hashed_mail}).toArray()
+
+    await collection.deleteMany({"mail" : mail})
+
+    return users
+}
+
+/**
+ * Get the informations of a user, according to its pseudo
+ * @param {string} pseudo : the pseudo used by the user
+ * @returns the info for the user, with following format {"pseudo" : pseudo, "mail" : hashed_mail}
+ */
+async function get_user(pseudo){
+    const db = client.db('sinf_server');
+    const collection = db.collection('users');
+
+    let user = await collection.findOne({"pseudo" : pseudo})
+    return user
 }
 
 
@@ -115,9 +129,10 @@ module.exports = {
     "connect" : connect,
     "close" : close,
     "add_ban" : add_ban,
-    'add_user' : add_user,
     "check_banned" : check_banned,
+    'add_user' : add_user,
     "remove_user" : remove_user,
+    "get_user" : get_user,
     "hash_mail" : hash_mail
 }
 
